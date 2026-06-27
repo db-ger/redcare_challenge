d446434f76e745abaa2e9ffb3124d934d8d5e9aa
db-ger <email@example.com>
2026-06-27 10:20:10 +0200

    pmd

--- src/main/java/com/redcare/challenge/exception/GithubApiException.java
+++ src/main/java/com/redcare/challenge/exception/GithubApiException.java
@@ -1,6 +1,10 @@
 package com.redcare.challenge.exception;
 
+import java.io.Serial;
+
 public class GithubApiException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 4819273506147852031L;
     public GithubApiException(String message) {
         super(message);
     }
